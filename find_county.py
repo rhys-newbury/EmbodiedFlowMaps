561ed838cd55b8ba715192727a31844db01d447e
Rhys Newbury <renew3@student.monash.edu>
2019-06-27 14:05:54 +1000

    Updated data scraping code

--- find_county.py
+++ find_county.py
@@ -1,16 +1,31 @@
 import urllib3
 import re
-
+d = []
 list = []
 succ = 0
+duplicate = 0
 fail =  0
+header = ''
+prevLat = -1
+prevLong = -1
+state_name  = ""
+county_name = ""
+
 with open('UPS network data v2 2019-4-23/lat2.csv') as f:
 
     for line in f:
+        if "Facility Latitude" in line:
+                header = line
+                continue
+        d.append(line.split(","))
+
+    d = sorted(d, key=lambda x:(float(x[10]), float(x[11])))
+
+    for data in d:
         try:
-            data = line.split(",")
+            line = ",".join(data)
             lat = data[10]
-            long = data[11]
+            long = data[11]               
 
             if (lat ==  '0.0' or long == '0.0'  or lat == '0' or long == '0'):
 
@@ -19,25 +34,40 @@ with open('UPS network data v2 2019-4-23/lat2.csv') as f:
             elif not lat.replace("-","").replace(".","").isdigit() and  not long.replace("-","").replace(".","").isdigit():
                 fail += 1
 
+            elif prevLong == long and prevLat == lat:
+
+                line = line.strip()
+
+
+                line += "," + state_name + "," + county_name
+                duplicate += 1
+
+                
+
             else:
                 request = "https://geo.fcc.gov/api/census/block/find?latitude=" + str(lat) + "&longitude=" + str(long) + "&format=xml"
 
                 http = urllib3.PoolManager()
                 r = http.request('GET', request)
 
-                match = re.search('name="(.*?)"', str(r.data))
+                match = re.findall('name="(.*?)"', str(r.data))
 
-                county_name = match.group(1)
+
+                county_name = match[0]
+                state_name = match[1]
 
                 line = line.strip()
-                line += "," + county_name
+                line += "," + state_name + "," + county_name
                 succ += 1
+
+                prevLong = long
+                prevLat = lat
 
             list.append(line)
 
 
-            if (succ+fail) % 10 == 0:
-                print(succ,fail)
+            if (succ+fail+duplicate) % 10 == 0:
+                print(duplicate, succ,fail)
 
 
 
