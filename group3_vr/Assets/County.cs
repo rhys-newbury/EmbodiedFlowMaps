4ee8419da7e7dd67329d4689aec4d4d4330233dc
Jesse <jabol6@student.monash.edu>
2019-08-21 10:55:10 +1000

    Some animation implementation

--- group3_vr/Assets/County.cs
+++ group3_vr/Assets/County.cs
@@ -14,7 +14,21 @@ public class County : PointableObject
 
     private List<GameObject> buildings = new List<GameObject>();
 
-    
+    public float speed = 0.05f;
+
+    GameObject cube;
+    GameObject dataCube;
+    GameObject dummyScaler;
+
+
+    private float startTime;
+
+    private float journeyLength;
+
+    private float journeyLengthData;
+
+
+
 
 
 
@@ -23,9 +37,66 @@ public class County : PointableObject
     {
         base.Start();
         Debug.Log("County Stuff");
+        cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        dataCube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        dummyScaler = GameObject.CreatePrimitive(PrimitiveType.Cube);
+
+        startTime = Time.time;
+        
         createBuildings();
 
+        createDataBuildings();
+
+        journeyLength = Vector3.Distance(cube.transform.localScale, new Vector3(0.01f, 0.5f, 0.01f));
+
+        journeyLengthData = Vector3.Distance(dataCube.transform.localScale, new Vector3(0.01f, 0.2f, 0.01f));
+
+
+
+
+
+
+    }
+
+
+    public void Update()
+    {
+        float distCovered = (Time.time - startTime) * speed;
+
+        float distCoveredData = (Time.time - startTime) * speed;
+
+        float fracJourney = distCovered / journeyLength;
+
+        float fracJourneyData = distCoveredData / journeyLengthData;
+
+        cube.transform.localScale = Vector3.Lerp(cube.transform.localScale, new Vector3(0.01f, 0.5f, 0.01f), fracJourney);
+
+        //dataCube.transform.localScale = Vector3.Lerp(cube.transform.localScale, new Vector3(0.01f, 0.2f, 0.01f), 0.5f);
+
+        //dummyScaler.transform.localScale = Vector3.Lerp(dummyScaler.transform.localScale, new Vector3(0f, 0.1f, 0f), fracJourney);
+
+
+
+
+        Vector3 temp = dataCube.transform.localScale;
+        Vector3 temp2 = dataCube.transform.localPosition;
+        if(fracJourney > 0.09f)
+        {
+            if (temp.y < 2f)
+            {
+                temp.y += 0.1f;
+                temp2.y += 0.05f;
+                
+
+                dataCube.transform.localScale = temp;
+                dataCube.transform.localPosition = temp2;
+
+            }
+        }
         
+
+        
+
     }
 
     public override Quaternion getAngle()
@@ -47,6 +118,10 @@ public class County : PointableObject
 
     public void createBuildings()
     {
+
+
+        startTime += 0.1f;
+
         string file = "C:\\Users\\Jesse\\Documents\\group3_vr\\group3_vr\\dummycounty.txt";
 
         StreamReader inp_stm = new StreamReader(file);
@@ -68,13 +143,19 @@ public class County : PointableObject
                 strData.Add(str);
             }
 
-            GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+
+            //journeyLength = Vector3.Distance(cube.transform.localScale, new Vector3(0.01f, 0.5f, 0.01f));
+
 
 
 
             cube.GetComponent<MeshRenderer>().material = Resources.Load("Materials/Glass", typeof(Material)) as Material;
-               
-            cube.name = "BUILDING";
+
+            //dataCube.GetComponent<MeshRenderer>().material = Resources.Load("Materials/GlowingGreen", typeof(Material)) as Material;
+
+            cube.name = "CONTAINER";
+            dataCube.name = "DATA";
+            dummyScaler.name = "SCALER";
 
             (x, y) = mapRenderer.convert(float.Parse(strData[0]), float.Parse(strData[1]));
 
@@ -83,9 +164,11 @@ public class County : PointableObject
 
             cube.transform.position = this.transform.parent.position;
 
+            //dataCube.transform.position = this.transform.parent.position;
 
 
-            cube.transform.localScale = new Vector3(0.01f, 0.5f, 0.01f);
+            //cube.transform.localScale = new Vector3(0.01f, 0.5f, 0.01f);
+
 
             
 
@@ -96,4 +179,28 @@ public class County : PointableObject
 
     }
 
+    public void createDataBuildings()
+    {
+        dataCube.transform.position = this.transform.parent.position;
+
+        dataCube.transform.position += new Vector3(0f, -0.1f, 0f);
+
+        dataCube.transform.localScale = new Vector3(0.01f, 0.01f, 0.01f);
+
+        Vector3 pos  = dataCube.transform.position;
+
+
+
+        dataCube.GetComponent<MeshRenderer>().material = Resources.Load("Materials/GlowingGreen", typeof(Material)) as Material;
+
+        dummyScaler.transform.position = pos += new Vector3(0f, -0.16f, 0f);
+
+        dummyScaler.transform.localScale = new Vector3(0.01f, 0.1f, 0.01f);
+
+        dataCube.transform.SetParent(dummyScaler.transform);
+
+
+    }
+
 }
+
