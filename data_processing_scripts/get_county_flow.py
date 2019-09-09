216e74929395257d5516a8bbf1e7db9efacc25d8
Rhys <rhys.newbury@monashmotorsport.com>
2019-09-10 02:45:42 +1000

    Processed flow countys

--- data_processing_scripts/get_county_flow.py
+++ data_processing_scripts/get_county_flow.py
@@ -17,12 +17,12 @@ with open('data_with_counties.csv') as f:
         except:
             print("fail")
             
-print(d)
        
 with open('../UPS network data v2 2019-4-23/vol2.csv') as f:
+    
     for line in f:
         data = line.split(",")
-
+    
         if data[0].strip() == "US" and data[2].strip() == "US":
             if data[1].strip() in d and data[3].strip() in d:
                 inc[d[data[1].strip()]] += float(data[4].strip())
@@ -31,16 +31,15 @@ with open('../UPS network data v2 2019-4-23/vol2.csv') as f:
                 
 
 
-x = open('inc.csv', 'w')
+x = open('county_in.csv', 'w')
 x.write("\n".join([",".join([key, str(val)]) for key, val in inc.items()]))
 x.close()
 
-x = open('out.csv', 'w')
+x = open('county_out.csv', 'w')
 x.write("\n".join([",".join([key, str(val)]) for key, val in out.items()]))
 x.close()
 
-
-x = open('flow.csv', 'w')
+x = open('county_flow.csv', 'w')
 x.write("\n".join([",".join([key, str(val)]) for key, val in flow.items()]))
 x.close()
 
